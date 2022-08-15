52825511563126266df4f0a380e3e0d9c6a48ca9
Helen Lehrer <hlehrer@outlook.com>
2022-08-15 10:47:57 -0700

    Add elements to HTML
     Co-authored-by: Trevor Hunter <trevorh8700@gmail.com>
     Co-authored-by: Noah Lundquist <nalundquist@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,28 @@ img.after(p1)
 img.setAttribute("src", "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900");
 img.setAttribute("alt", "This is an image of a Multicolored Tanager bird from birdsoftheworld.org");
 img.setAttribute("width", "50%");
-//img.append("src='https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900' alt='This is an image of a Multicolored Tanager bird from birdsoftheworld.org' style='width:50%'")
-document.body.append(firsth1, p1, img)
+
+const secondh1 = document.createElement("h1");
+secondh1.append("Facts about the Multicolored Tanager");
+secondh1.after(img);
+
+const ul = document.createElement("ul");
+const firstLi = document.createElement("li");
+const secondLi = document.createElement("li");
+ul.after(secondh1);
+firstLi.append("It is endemic to the mountains of Colombia.");
+secondLi.append("It usually searches for insects on the underside of leaves of outer limbs while clinging to leaves with its feet.");
+ul.append(firstLi);
+secondLi.after(firstLi);
+
+const h2 = document.createElement("h2");
+h2.append("Source");
+h2.after(ul);
+
+const a = document.createElement("a");
+a.setAttribute("href", "https://en.wikipedia.org/wiki/Multicolored_tanager");
+a.append("Wikipedia");
+a.after("h2");
+
+document.body.append(firsth1, p1, img, secondh1, ul, firstLi, secondLi, h2, a)
 })
