9f81d2ff002248067537837f56c9274f0cc14043
Helen Lehrer <hlehrer@outlook.com>
2022-08-15 10:30:34 -0700

    Add image with javascript
     Co-authored-by: Trevor Hunter <trevorh8700@gmail.com>
    Co-authored-by: Noah Lundquist <nalundquist@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,11 @@ const p1 = document.createElement("p");
 p1.after(firsth1);
 p1.append("The HTML of this webpage was created with JavaScript.");
 
-
-document.body.append(firsth1, p1)
+const img = document.createElement("img");
+img.after(p1)
+img.setAttribute("src", "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900");
+img.setAttribute("alt", "This is an image of a Multicolored Tanager bird from birdsoftheworld.org");
+img.setAttribute("width", "50%");
+//img.append("src='https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900' alt='This is an image of a Multicolored Tanager bird from birdsoftheworld.org' style='width:50%'")
+document.body.append(firsth1, p1, img)
 })
